38e295be7491a6c8c15cdf03a6538d56d93e1d5d
tychodub <t.b.dubbeling@student.utwente.nl>
2024-09-20 17:10:44 +0200

    added testing file for NestedSubtypes

---
+++ src/UnsortedExamples/NestedSubtypes.java
@@ -0,0 +1,13 @@
+package UnsortedExamples;
+
+public class NestedSubtypes {
+    //@ subtype nat(int x)() = x >= 0;
+    //@ subtype oneHand(int y)() = y <= 10;
+    //@ subtype natEven(subtype<int, nat> x)() = (x % 2) == 0;
+    //@ subtype thirdLayer(subtype<int, natEven oneHand> y)() = (y == 4) || (y == 8);
+
+    public static void main(String[] args) {
+        /*@ natEven @*/ int x = 2;
+        x = 3;
+    }
+}
