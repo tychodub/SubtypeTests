bcb99b378965e2bfc7be8d11c826443aab2ac3c7
tychodub <t.b.dubbeling@student.utwente.nl>
2025-05-02 17:36:26 +0200

    added new test for booleanAlgebra in general, should be expanded for more extensive testing

---
+++ src/UnsortedExamples/SubtypeAlgebra.java
@@ -0,0 +1,14 @@
+package UnsortedExamples;
+
+public class SubtypeAlgebra {
+    //@ subtype nat(int x)() = x >= 0;
+    //@ subtype nullish(int[] x)() = x == null;
+    //@ subtype nonNull(int[] xs)() = xs!=null;
+    //@ subtype length(int[] xs)(int l) = xs!=null && xs.length==l;
+
+    public static void main(String[] args) {
+        /*@ nullish | length(10) | nonNull ==> length(3) @*/ int[] arr = null;
+        arr = new int[10];
+        arr = new int[3];
+    }
+}
