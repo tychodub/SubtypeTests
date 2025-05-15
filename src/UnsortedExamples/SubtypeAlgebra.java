9963c8d57053e317a5b1dd0c69c46d0459681515
tychodub <t.b.dubbeling@student.utwente.nl>
2025-05-15 15:31:47 +0200

    added some additional code to SubtypeAlgebra

--- src/UnsortedExamples/SubtypeAlgebra.java
+++ src/UnsortedExamples/SubtypeAlgebra.java
@@ -5,10 +5,13 @@ public class SubtypeAlgebra {
     //@ subtype nullish(int[] x)() = x == null;
     //@ subtype nonNull(int[] xs)() = xs!=null;
     //@ subtype length(int[] xs)(int l) = xs!=null && xs.length==l;
+    //@ subtype negOrZero(int x)() = x <= 0;
 
     public static void main(String[] args) {
         /*@ nullish | length(10) | nonNull ==> length(3) @*/ int[] arr = null;
         arr = new int[10];
         arr = new int[3];
+        /*@ nat !(negOrZero) @*/ int x = 1;
+        x = 0;
     }
 }
