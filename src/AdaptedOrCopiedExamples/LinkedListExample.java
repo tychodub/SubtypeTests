e0ff6d76756a1fa2e51c75fdcfb95f9ed0edbbd4
tychodub <t.b.dubbeling@student.utwente.nl>
2025-06-29 16:23:16 +0200

    adapted the LinkedListExample so that VerCors can verify it without strict-arithmetic and reject it without strict-arithmetic

--- src/AdaptedOrCopiedExamples/LinkedListExample.java
+++ src/AdaptedOrCopiedExamples/LinkedListExample.java
@@ -4,21 +4,39 @@ package AdaptedOrCopiedExamples;
 // this is a somewhat adapted monomorphic version of the relevant code, such that VerCors can handle it
 
 public class LinkedListExample {
-    //@ subtype i32(int x)() = -2147483648 <= x && x <= 2147483647;
+    //@ resource allPerms(Node x) = (x != null) ==> (Perm(x.next, write) ** Perm(x.item, write) ** allPerms(x.next));
 
     transient int size = 0;
     transient Node first;
     transient Node last;
 
-    public /*@ i32 @*/ int size() {
+    //@ requires Perm(size, read);
+    //@ ensures Perm(size, read);
+    public int size() {
         return size;
     }
 
+    //@ requires Perm(last, write);
+    //@ requires Perm(last.next, write);
+    //@ requires Perm(first, write);
+    //@ requires Perm(size, write);
+    //@ ensures Perm(last, write);
+    //@ ensures Perm(last.next, write);
+    //@ ensures Perm(first, write);
+    //@ ensures Perm(size, write);
     public boolean add(Object e) {
         linkLast(e);
         return true;
     }
 
+    //@ requires Perm(last, write);
+    //@ requires Perm(last.next, write);
+    //@ requires Perm(first, write);
+    //@ requires Perm(size, write);
+    //@ ensures Perm(last, write);
+    //@ ensures Perm(last.next, write);
+    //@ ensures Perm(first, write);
+    //@ ensures Perm(size, write);
     void linkLast(Object e) {
         final Node l = last;
         final Node newNode = new Node(l, e, null);
@@ -31,18 +49,25 @@ public class LinkedListExample {
         size++;
     }
 
+    //@ requires Perm(first, write);
+    //@ requires allPerms(first);
+    //@ ensures Perm(first, write);
     public int indexOf(Object o) {
         int index = 0;
         if (o == null) {
+            //@ loop_invariant allPerms(x);
             for (Node x = first; x != null; x = x.next) {
+                //@ unfold allPerms(x);
                 if (x.item == null) {
                     return index;
                 }
                 index++;
             }
         } else {
+            //@ loop_invariant allPerms(x);
             for (Node x = first; x != null; x = x.next) {
-                if (o.equals(x.item)) {
+                //@ unfold allPerms(x);
+                if (o == x.item) { // should be equals
                     return  index;
                 }
                 index++;
--- src/AdaptedOrCopiedExamples/Node.java
+++ src/AdaptedOrCopiedExamples/Node.java
@@ -4,6 +4,10 @@ public class Node {
     Object item;
     Node next;
     Node prev;
+
+    //@ ensures Perm(prev, write);
+    //@ ensures Perm(item, write);
+    //@ ensures Perm(next, write);
     Node(Node p, Object i, Node n) {
         prev = p;
         item = i;
