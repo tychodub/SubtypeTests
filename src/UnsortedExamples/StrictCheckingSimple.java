e8144efd794c1218a3be3d8b2d8231cc5d856d56
tychodub <t.b.dubbeling@student.utwente.nl>
2025-06-30 17:06:19 +0200

    made the avg method in StrictCheckingSimple actually calculate the average

--- src/UnsortedExamples/StrictCheckingSimple.java
+++ src/UnsortedExamples/StrictCheckingSimple.java
@@ -3,7 +3,7 @@ package UnsortedExamples;
 public class StrictCheckingSimple {
     //@ subtype range(int x)(int bottom, int top) = x >= bottom && x <= top;
     //@ subtype mulId(int x)() = x == 1;
-    //@ subtype nonNull(int[] xs)() = xs != null;
+    //@ subtype nonEmpty(int[] xs)() = xs!=null && xs.length>0;
 
     public static void main(String[] args) {
         /*@ strict range(0, 128) @*/ int x = (128+2)-2;
@@ -22,7 +22,7 @@ public class StrictCheckingSimple {
     }
 
     //@ requires Perm(xs[*], read);
-    private static int avg(/*@ nonNull @*/ int[] xs) {
+    private static int avg(/*@ nonEmpty @*/ int[] xs) {
         int result = 0;
         //@ loop_invariant 0 <= i && i <= xs.length;
         /*@ loop_invariant (\forall* int i; 0 <= i && i < xs.length;
@@ -30,6 +30,6 @@ public class StrictCheckingSimple {
         for (int i=0; i<xs.length; i++) {
             result += xs[i];
         }
-        return result;
+        return result/xs.length;
     }
 }
